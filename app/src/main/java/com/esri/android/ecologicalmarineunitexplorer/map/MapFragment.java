627568e8deb3c3a07d2158dc3c517fcb32c88832
Sandie Peters <sandie_peters@esri.com>
2016-10-13 15:32:57 -0700

    Uncommented lines that I forgot to uncomment before the previous check in

--- app/src/main/java/com/esri/android/ecologicalmarineunitexplorer/map/MapFragment.java
+++ app/src/main/java/com/esri/android/ecologicalmarineunitexplorer/map/MapFragment.java
@@ -12,7 +12,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -195,7 +197,8 @@ public class MapFragment extends Fragment implements MapContract.View {
   private void setUpToolbar() {
     final Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
     ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-    ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Explore An Ocean Location");
+    ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+    actionBar.setTitle("Explore An Ocean Location");
   }
 
   @Override
